6ccc57385680da9c8f9efc5cc7e6a73cffe65ca2
JosephThweatt <josephthweatt@gmail.com>
2018-09-04 09:23:18 -0700

    Part 3: Button makes an API call and sets the Image

--- react-native-pokemon-app/App.js
+++ react-native-pokemon-app/App.js
@@ -1,10 +1,76 @@
 import React from 'react';
-import {StyleSheet, Image, Button, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View, Image} from 'react-native';
+import axios from 'axios'
+
+/**
+ * This is a new Component that will be used in our primary
+ * Component (App). This is implemented in App by using
+ * <Info />
+ */
+class Info extends React.Component {
+
+    state = {
+        data: {
+            imgSrc: require('./images/pokeball.png'),
+            name: 'Name',
+            type: 'Type',
+        }
+    };
+
+    /**
+     * This function comes from React.Component and will be called whenever
+     * this Info component receives different props. Here, we use the new
+     * to set the 'state'. Changes made to the state causes render() to
+     * get called again.
+     *
+     * @param newProps - an object containing the new props getting set
+     */
+    componentWillReceiveProps(newProps) {
+        console.log(newProps.data);
+        this.setState({data: newProps.data});
+    }
+
+    render() {
+        return (
+            <View style={styles.info}>
+                <Image source={this.state.data.imgSrc} style={styles.image}/>
+                <Text style={styles.text}>{this.state.data.name}</Text>
+                <Text style={styles.text}>{this.state.data.type}</Text>
+            </View>
+        );
+    }
+}
 
 export default class App extends React.Component {
 
+    state = {};
+
     getPokemon = () => {
-        // leave this blank for now
+
+        const app = this;
+
+        // Okay, the pokemon API is down, so I'm replacing it with this API for doggos
+        axios.get('https://dog.ceo/api/breeds/image/random').then(function (response) {
+
+            /**
+             * Within 'response' is a value called 'data', which can be referenced by calling
+             * 'response.data' That will return an object structured like this:
+             *      {
+             *          status: "success",
+             *          message: "http://url/to/an/image"
+             *      }
+             *
+             *  We grab the url found in 'message' and include that in the 'data' object
+             *  created below, which in turn is set within this Component's state.
+             */
+            let data = {
+                imgSrc: {uri: response.data.message},
+                name: 'LOOK AT HIM!!!',
+                type: 'Such a good Boy!'
+            };
+
+            app.setState({data});
+        });
     };
 
     /**
@@ -21,11 +87,10 @@ export default class App extends React.Component {
      *          function, which is executed when the button is pressed
      */
     render() {
+
         return (
             <View style={styles.container}>
-                <Image source={require('./images/pokeball.png')} style={styles.image}/>
-                <Text style={styles.text}>Name</Text>
-                <Text style={styles.text}>Type</Text>
+                <Info data={this.state.data}/>
                 <Button title="Get Random Pokemon" color='#d00' style={styles.button}
                         onPress={() => {
                             this.getPokemon()
@@ -52,6 +117,12 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         marginTop: 50
+    },
+    info: {
+        flex: .75,
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginBottom: 75
     },
     image: {
         height: 200,
